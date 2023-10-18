6d8d0aca1a0329584aa3d3b497b056745858a1bc
valenpe <valentinaperez230701@gmail.com>
2023-10-18 01:50:18 -0300

    push25

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,6 +4,7 @@ export default function About() {
     <div>
         <h1>ABOUT</h1>
         <h3>GIOVANNI SOS EL MEJOR PERUANO QUE CONOZCO</h3>
+        <p>¿Que queres que te diga?</p>
     </div>
   )
 }
