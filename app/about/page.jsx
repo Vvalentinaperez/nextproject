5414c5bbb31bdf1e1e768ae3b20b680720870496
GCespedes <giovannice22@hotmail.com>
2023-10-17 23:38:19 -0500

    prueba commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,14 @@
-
 export default function About() {
   return (
     <div>
-        <h1>ABOUT</h1>
+      <h1>ABOUT</h1>
+      <ul>
+        <li>Hola</li>
+        <li>Como</li>
+        <li>te va</li>
+        <li>el dia</li>
+        <li>:D</li>
+      </ul>
     </div>
-  )
+  );
 }
